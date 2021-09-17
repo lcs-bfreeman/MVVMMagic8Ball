a95301b958ae84b4fde4fb24d492978bbc8453aa
Benjamn <bfreeman@lcs.on.ca>
2021-09-17 11:51:22 -0400

    Converted The array into a function

---
+++ MVVMMagic8Ball/Model/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MVVMMagic8Ball
+//
+//  Created by Ben Freeman on 2021-09-17.
+//
+
+import Foundation
--- MVVMMagic8Ball/main.swift
+++ MVVMMagic8Ball/main.swift
@@ -42,29 +42,7 @@ let value = Int.random(in: 1...20)
 //      Command-Shift-L
 //
 
-let responses = [
-    "It is certain",
-    "It is decidely so.",
-    "Without a doubt.",
-    "Yes definitely.",
-    "You may rely on it.",
-    "As I see it, yes.",
-    "Most likely.",
-    "Outlook good.",
-    "Yes.",
-    "Signs point to yes.",
-    "Reply hazy, try again.",
-    "Ask again later.",
-    "Better not to tell you now.",
-    "Cannot predict now.",
-    "Concentrate and ask again.",
-    "Don't count on it.",
-    "My reply is no.",
-    "My sources say no.",
-    "Outlook not so good.",
-    "Very doubtful.",
-    
-]
+
 
 
 // Provide the advice
